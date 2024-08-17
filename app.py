0fb263527a74c3ca52cf83d8357bf4e113ae8843
Cuchufito <nahuellugo123@gmail.com>
2024-08-17 13:15:00 -0400

    Probando las Rutas

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
-app = Flask (__name__)
+app = Flask(__name__)
 
 @app.route('/inicio')
 def inicio():
     return "hola mundo desde el backend"
-#endpoint o ruta 
+
+# endpoint o ruta
 @app.route('/contacto')
 def contacto():
     return "<h3>Introduciendo HTML desde el servidor</h3>"
@@ -13,5 +14,14 @@ def contacto():
 @app.route('/contacto2')
 def contacto2():
     return render_template('contacto.html')
-if __name__ =='__main__':
+
+@app.route('/guardarMascota', methods=['POST'])
+def guardarMascota():
+    print(request.form)
+    nombreMascota = request.form.get('txtNombreMascota')
+    return f"Ya llegó tu mascota <strong>{nombreMascota}</strong> al servidor"
+
+
+# se pregunta por el proceso principal
+if __name__=='__main__':
     app.run(debug=True)
